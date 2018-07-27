cd77c78ef09563e61bb5e17317ee225d9ce6f767
liuyinxin <honor@liuyinxin.com>
2018-07-27 16:34:37 +0800

    '删除链表中的节点'

---
+++ src/com/liuyinxin/list/删除链表中的节点.java
@@ -0,0 +1,25 @@
+package com.liuyinxin.list;
+
+public class 删除链表中的节点 {
+
+    public static void main(String[] args) {
+        删除链表中的节点 s = new 删除链表中的节点();
+        ListNode listNode = new ListNode(4);
+        listNode.next = new ListNode(5);
+        listNode.next.next = new ListNode(1);
+        listNode.next.next.next = new ListNode(9);
+        s.deleteNode(listNode.next.next.next);
+
+        while (listNode != null){
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+
+    }
+
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
