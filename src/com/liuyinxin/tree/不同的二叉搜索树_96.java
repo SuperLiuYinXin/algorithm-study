7f1f1972211599e431d2a2952b92cca7efb02efd
liuyinxin <honor@liuyinxin.com>
2019-04-16 21:20:26 +0800

    不同的二叉搜索树_96

--- src/com/liuyinxin/tree/不同的二叉搜索树_96.java
+++ src/com/liuyinxin/tree/不同的二叉搜索树_96.java
@@ -3,7 +3,7 @@ package com.liuyinxin.tree;
 /**
  *
  * 不同的二叉搜索树的个数
- * 是一个卡特兰数,学习的链接 <a href="http://lanqi.org/skills/10939/">卡特兰数</a>
+ * 是一个<strong>卡特兰数</strong>,学习的链接 <a href="http://lanqi.org/skills/10939/">卡特兰数</a>
  * <br/>
  * 通项公式为　C1 = 1 C(n+1) = Sum( C(i) * C(n - i)) (s.t  i = 0...n)
  * 但是这个只能求解个数
@@ -15,4 +15,19 @@ package com.liuyinxin.tree;
  * do the best
  */
 public class 不同的二叉搜索树_96 {
+
+    public int numTrees(int n) {
+        if(n== 0) return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+
 }
